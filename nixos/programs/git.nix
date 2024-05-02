6bf240aecb91b6eb0ff5ab3677c40da93b3843e5
declan <declan@ai-nc.com>
2024-05-02 13:37:47 +1000

    trying to add gh config

--- nixos/programs/git.nix
+++ nixos/programs/git.nix
@@ -1,9 +1,16 @@
 # programs.git = 
+let
+  gh_auth = "!/run/current-sustem/sw/bin/gh auth git-credential";
+in
 pkgs: {
   enable = true;
   package = pkgs.gitFull;
   userName = "declan";
   userEmail = "declan@ai-nc.com";
-  extraConfig.pull.rebase = false;
-  extraConfig.credential.helper = "oauth";
+  extraConfig = {
+    pull.rebase = false;
+    credential.helper = "oauth";
+    "credential \"https://github.com\"".helper = gh_auth;
+    "credential \"https://gist.github.com\"".helper = gh_auth;
+  };
 }
